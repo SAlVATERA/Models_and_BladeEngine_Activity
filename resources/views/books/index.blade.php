093b65b70cb2a13547ad9bfc142c2d4cac4e2f7c
jasonsalvatera <jasonsalvatera24680@gmail.com>
2024-04-12 16:36:46 +0800

    added books controller and the respected blade.php file, also edited the contents of both and therespected routes

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mx-auto">
+        <h1 class="text-3xl font-bold mb-4">Book List</h1>
+        <table class="min-w-full">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>ISBN</th>
+                    <th>Date Published</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($books as $book)
+                <tr>
+                    <td>{{ $book->title }}</td>
+                    <td>{{ $book->author }}</td>
+                    <td>{{ $book->isbn }}</td>
+                    <td>{{ $book->date_published }}</td>
+                    <td>{{ $book->description }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
